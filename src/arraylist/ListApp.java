9be47d0c8d772561069f3f1ae9cab8317d4df6da
lschoch@gmail.com <lschoch@gmail.com>
2021-11-13 19:59:19 -0500

    Modify output.

--- src/arraylist/ListApp.java
+++ src/arraylist/ListApp.java
@@ -3,6 +3,7 @@ package arraylist;
 public class ListApp {
 
 	public static void main(String[] args) {
+		
 		ArrayList<String> stringList = new ArrayList<String>();
 		ArrayList<Square> squareList = new ArrayList<Square>();
 		ArrayList<PointThreeD> pointList = new ArrayList<PointThreeD>();
@@ -11,20 +12,25 @@ public class ListApp {
 		stringList.addItem("Second string.");
 		stringList.addItem("Fourth string.");
 		stringList.addItem("What happened to the \"third string\"?");
+		System.out.println("\nstringList:");
+		System.out.println("-----------");
+		System.out.println(stringList.toString());
 
 		squareList.addItem(new Square(3.445));
 		squareList.addItem(new Square(65.67));
 		squareList.addItem(new Square(23456.0));
 		squareList.addItem(new Square(102.2345));
+		System.out.println("\nsquareList:");
+		System.out.println("-----------");
+		System.out.println(squareList.toString());
 
 		pointList.addItem(new PointThreeD(1.2, 1.4, 2.4));
 		pointList.addItem(new PointThreeD(23.5, 12.456, 9.0));
 		pointList.addItem(new PointThreeD(4, 5, 6));
 		pointList.addItem(new PointThreeD(100.00, 345.67, 2.3));
-		
-		System.out.println("\n"+stringList.toString());
-		System.out.println("\n"+squareList.toString());
-		System.out.println("\n"+pointList.toString());
+		System.out.println("\npointList:");
+		System.out.println("----------");
+		System.out.println(pointList.toString());
 
 	}//end main
 
